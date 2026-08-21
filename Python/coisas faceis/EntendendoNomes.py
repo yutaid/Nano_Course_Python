4fc439d08a3b26a696e9040d97626b3e4cd99475
Yutaid <mateusyutadp@gmail.com>
2026-08-21 13:00:56 -0300

    belo nome

--- Python/coisas faceis/EntendendoNomes.py
+++ Python/coisas faceis/EntendendoNomes.py
@@ -2,4 +2,4 @@ print("Entendendo Nomes")
 primeiro_nome = input("Primeiro nome: ")
 sobrenome = input("Sobrenome: ")
 nome_completo = primeiro_nome + " " + sobrenome
-print(f"Seu nome completo é {nome_completo}")
+print(f"Seu nome completo é {nome_completo}, belo nome")
