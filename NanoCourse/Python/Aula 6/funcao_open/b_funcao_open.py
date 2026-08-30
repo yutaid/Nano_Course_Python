d15516644d4f6700216f1c2594ad07a94d478193
Yutaid <mateusyutadp@gmail.com>
2026-08-29 21:12:28 -0300

    aula 6

--- NanoCourse/Python/Aula 6/funcao_open/b_funcao_open.py
+++ NanoCourse/Python/Aula 6/funcao_open/b_funcao_open.py
@@ -1,14 +1,16 @@
 #usando a função open para criar um objeto do tipo arquivo
 
-arquivo = open("d:\\mateu\\algo\\arquivo.txt")
+arquivo = open("d:\\mateu\\algo\\arquivo.txt", "r", encoding="UTF-8")
+print(type(arquivo))
+print(" ")
 
 #verificando o tipo do objeto arquivo
 
-
+print(arquivo)
 
 #printando o objeto arquivo
 
-
+print(arquivo.read())
 
 #printando o conteúdo do objeto arquivo
 
